27cd7f21916b6a8d1655811be801753422aa297d
luccr1 <lucca.hellriegel@gmx.net>
2020-07-09 10:46:57 +0200

    removed workbench config

--- src/App.tsx
+++ src/App.tsx
@@ -10,22 +10,6 @@ import { page } from "./components/ReaderControl/ReaderControlActionsReducers";
 import { numPages, pageData } from "./components/Reader/ReaderActionsReducers";
 import thunk from "redux-thunk";
 
-// const initialState: { workbenchGrid: GridSetup } = {
-// 	workbenchGrid: {
-// 		nextID: 5,
-// 		slot1: {
-// 			slotType: "Elements",
-// 			slotContent: [
-// 				{ elementType: "Note", id: 0 },
-// 				{ elementType: "Note", id: 4 },
-// 			],
-// 		},
-// 		slot2: { slotType: "Elements", slotContent: [{ elementType: "QA-Card", id: 1 }] },
-// 		slot3: { slotType: "Material", slotContent: "PDF" },
-// 		slot4: { slotType: "Elements", slotContent: [{ elementType: "Blanks-Card", id: 3 }] },
-// 	},
-// };
-
 const rootReducer = combineReducers({ pdf, pageData, section, page, numPages });
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
