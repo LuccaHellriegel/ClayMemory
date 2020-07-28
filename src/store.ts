48b9898e42680b50b51087123425df251773a97a
luccr1 <lucca.hellriegel@gmx.net>
2020-07-28 17:10:03 +0200

    misc

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,18 @@
 import { createStore } from "redux";
-import { rootReducer } from "./rootReducer";
 import { applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import { combineReducers } from "redux";
+import modules from "./modules";
+
+const rootReducer = combineReducers({
+	[modules.control.constants.NAME]: modules.control.reducer,
+	[modules.cards.creation.constants.NAME]: modules.cards.creation.reducer,
+	[modules.cards.river.constants.NAME]: modules.cards.river.reducer,
+	[modules.material.analyze.constants.NAME]: modules.material.analyze.reducer,
+	[modules.material.display.constants.NAME]: modules.material.display.reducer,
+	[modules.material.select.constants.NAME]: modules.material.select.reducer,
+});
 
 const stateSanitizer = (state: any) => {
 	let newState = state.displayData.pdf
