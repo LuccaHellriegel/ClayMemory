f0451b398bc74045e58d4b1b13c2abe870a5d9f4
luccr1 <lucca.hellriegel@gmx.net>
2020-08-07 19:44:51 +0200

    Updates

---
+++ precache-manifest.fcde804eba2e29058b9f14cb759d7144.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f589129ecbeeddef20dc91ac74823b4",
+    "url": "/ClayMemory/index.html"
+  },
+  {
+    "revision": "0adc888748617f97db50",
+    "url": "/ClayMemory/static/css/main.c123f778.chunk.css"
+  },
+  {
+    "revision": "9163339723cd94574ab5",
+    "url": "/ClayMemory/static/js/2.7c629608.chunk.js"
+  },
+  {
+    "revision": "0463dfe48cf07122e65283f3f22f9c80",
+    "url": "/ClayMemory/static/js/2.7c629608.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0adc888748617f97db50",
+    "url": "/ClayMemory/static/js/main.23cae690.chunk.js"
+  },
+  {
+    "revision": "4bc9783f1087e77165cb",
+    "url": "/ClayMemory/static/js/runtime-main.1c2a598d.js"
+  }
+]);
