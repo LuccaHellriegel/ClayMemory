81a05184c49f87576f8cd4bbf2588cc9253580c6
luccr1 <lucca.hellriegel@gmx.net>
2020-08-07 17:11:48 +0200

    refactored keyboardControl to use keyMaps

--- src/modules/control/services/keyboardControl.ts
+++ src/modules/control/services/keyboardControl.ts
@@ -1,21 +1,66 @@
 import { Dispatch } from "redux";
-import { pageControl, sectionControl, selectionControl } from "./focus/selectionFocus";
-import { contextMenuControl } from "./focus/sharedFocus";
 import focus from "../../focus";
+import display from "../../display";
+import select from "../../select";
+import creation from "../../creation";
+import { UserFocus } from "../../focus/model";
+
+type KeyActionMap = { [key: string]: any };
+
+const pageControlKeyMap: KeyActionMap = {
+	ArrowLeft: display.actions.previousPage(),
+	ArrowRight: display.actions.nextPage(),
+};
+
+type KeyEventDispatcher = (event: KeyboardEvent, dispatch: any) => void;
+
+const keyEventDispatcher = (keyMap: KeyActionMap) => (event: KeyboardEvent, dispatch: any) => {
+	const action = keyMap[event.key];
+	if (action) {
+		event.preventDefault();
+		dispatch(action);
+	}
+};
+
+const sectionControlKeyMap: KeyActionMap = {
+	ArrowDown: select.actions.updateSection("DOWN"),
+	ArrowUp: select.actions.updateSection("UP"),
+	" ": select.actions.toggleSectionMovementState(),
+};
+
+const selectionControlKeyMap: KeyActionMap = {
+	a: select.actions.updateSelection("PLUS_WORD"),
+	d: select.actions.updateSelection("MINUS_WORD"),
+	w: select.actions.updateSelection("PLUS_SPAN"),
+	s: select.actions.updateSelection("MINUS_SPAN"),
+};
+
+const contextMenuControlKeyMap: KeyActionMap = {
+	t: creation.actions.toggleContextMenu(),
+};
+
+const selectionFocusKeyMap = {
+	...pageControlKeyMap,
+	...sectionControlKeyMap,
+	...selectionControlKeyMap,
+	...contextMenuControlKeyMap,
+};
+const selectionFocusDispatcher = keyEventDispatcher(selectionFocusKeyMap);
+
+const contextMenuFocusKeyMap = { ...contextMenuControlKeyMap };
+const contextMenuFocusDispatcher = keyEventDispatcher(contextMenuFocusKeyMap);
+
+const focusDispatcherMap: { [focus in UserFocus]: KeyEventDispatcher } = {
+	SELECTION: selectionFocusDispatcher,
+	CONTEXT_MENU: contextMenuFocusDispatcher,
+	EDITOR: (event, dispatch) => {},
+};
 
 export const keyboardControl = (event: KeyboardEvent) => {
 	return (dispatch: Dispatch | any, getState: Function) => {
 		const userFocus = focus.selectors.getFocus(getState());
-		switch (userFocus) {
-			case "SELECTION":
-				pageControl(event.key, event, dispatch);
-				sectionControl(event.key, event, dispatch);
-				selectionControl(event.key, event, dispatch);
-				contextMenuControl(event.key, event, dispatch);
-				break;
-			case "CONTEXT_MENU":
-				contextMenuControl(event.key, event, dispatch);
-				break;
-		}
+		const dispatcher = focusDispatcherMap[userFocus];
+
+		dispatcher(event, dispatch);
 	};
 };
