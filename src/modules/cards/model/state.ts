eb5f94ec03ca333157747c5721f6b6a609583704
luccr1 <lucca.hellriegel@gmx.net>
2020-10-17 00:25:14 +0200

    inline replaceCardsInCardsState

--- src/modules/cards/model/state.ts
+++ src/modules/cards/model/state.ts
@@ -14,16 +14,9 @@ export const removeCardFromCardsState = (state: CardsState, cardID: CardID) => {
 
 	return { ...state, cards };
 };
-export const replaceCardsInCardsState = (state: CardsState, cards: Cards, lastCardIDNumber: number) => {
-	return {
-		...state,
-		cards,
-		lastCardIDNumber,
-	};
-};
+
 export const updateCardInCards = (cards: Cards, toBeUpdated: CardConfig) => {
 	const cardObj = { ...cards };
 	cardObj[toBeUpdated.cardID] = toBeUpdated;
 	return cardObj;
 };
-export const addCardToCards = updateCardInCards;
--- src/modules/cards/reducer.ts
+++ src/modules/cards/reducer.ts
@@ -1,12 +1,6 @@
 import * as t from "./actionTypes";
 import { cardIDToNumber } from "./model/config";
-import {
-	CardsState,
-	removeCardFromCardsState,
-	updateCardInCards,
-	addCardToCards,
-	replaceCardsInCardsState,
-} from "./model/state";
+import { CardsState, removeCardFromCardsState, updateCardInCards } from "./model/state";
 import { ArchiveCards } from "../db/model";
 import db from "../db";
 import { ClayMemoryPayloadAction } from "../../shared/utils";
@@ -20,18 +14,18 @@ const cards = (state = intialState, { type, payload }: ClayMemoryPayloadAction) 
 	switch (type) {
 		case t.CARD_PUSH:
 			const lastCardIDNumber = cardIDToNumber(payload.cardID) + 1;
-			return { ...state, cards: addCardToCards(state.cards, payload), lastCardIDNumber };
+			return { ...state, cards: updateCardInCards(state.cards, payload), lastCardIDNumber };
 		case t.CARD_REPLACE:
 			return { ...state, cards: updateCardInCards(state.cards, payload) };
 		case t.CARD_REMOVE:
 			return removeCardFromCardsState(state, payload as string);
 		case db.actionTypes.DOCUMENT_CHANGE:
 			if (payload) {
-				return replaceCardsInCardsState(
-					intialState,
-					(payload as ArchiveCards).cards,
-					(payload as ArchiveCards).lastCardIDNumber
-				);
+				return {
+					...intialState,
+					cards: (payload as ArchiveCards).cards,
+					lastCardIDNumber: (payload as ArchiveCards).lastCardIDNumber,
+				};
 			} else {
 				return intialState;
 			}
@@ -40,11 +34,11 @@ const cards = (state = intialState, { type, payload }: ClayMemoryPayloadAction) 
 			// when uploading data that corresponds to current document
 			// and if not we dont reset
 			if (payload.newActiveDataSet) {
-				return replaceCardsInCardsState(
-					intialState,
-					(payload as ArchiveCards).cards,
-					(payload as ArchiveCards).lastCardIDNumber
-				);
+				return {
+					...intialState,
+					cards: (payload as ArchiveCards).cards,
+					lastCardIDNumber: (payload as ArchiveCards).lastCardIDNumber,
+				};
 			} else {
 				return state;
 			}
