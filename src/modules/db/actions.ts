fb61f6f6783c43548afa3d18ec78b178b488e698
luccr1 <lucca.hellriegel@gmx.net>
2020-10-18 22:33:19 +0200

    use actualized state when downloading datasets

--- src/modules/db/actions.ts
+++ src/modules/db/actions.ts
@@ -5,6 +5,7 @@ import { collectCurrentDBData, getDocumentDataSets } from "./selectors";
 import { actions } from "./slice";
 
 export const refreshDB = (dispatch: Dispatch, state: any) => {
+	// note: need to getState again after this to make sure refreshed state is used
 	const currentPDFName = pdf.selectors.getPDFName(state);
 	if (currentPDFName !== undefined) {
 		const dbData = collectCurrentDBData(state) as DocumentData;
@@ -15,10 +16,12 @@ export const refreshDB = (dispatch: Dispatch, state: any) => {
 const fileDownload = require("js-file-download");
 export const downloadDBData = () => {
 	return (dispatch: Dispatch, getState: Function) => {
-		const state = getState();
+		let state = getState();
 
 		//actualize the db before downloading
 		refreshDB(dispatch, state);
+		// get actualized state
+		state = getState();
 
 		const documentDataSets = getDocumentDataSets(state);
 		const localString = new Date().toLocaleString();
