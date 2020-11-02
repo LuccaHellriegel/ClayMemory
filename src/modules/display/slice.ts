b07e8ccfe0cad949c4cf43dc13fb4e1b5972821e
luccr1 <lucca.hellriegel@gmx.net>
2020-11-02 23:29:44 +0100

    change to RiverExplorer if cards exist

--- src/modules/display/slice.ts
+++ src/modules/display/slice.ts
@@ -3,6 +3,8 @@ import { simpleReducer } from "../../shared/utils";
 import { CaseReducer, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { NAME } from "./constants";
 import cards from "../cards";
+import { CardsState } from "../cards/model/state";
+import pdf from "../pdf";
 
 const initialState: DisplayData = {
 	windowMeasurements: null,
@@ -15,9 +17,16 @@ const windowMeasurements: CaseReducer<
 	DisplayData,
 	PayloadAction<{ width: number; height: number } | null>
 > = simpleReducer("windowMeasurements");
+
 const currentView: CaseReducer<DisplayData, PayloadAction<View>> = simpleReducer("currentView");
-const explorerView: CaseReducer<DisplayData> = (state) => {
-	return { ...state, currentView: View.RiverExplorer };
+
+//TODO: suboptimal, changes to RiverExplorer also if I upload pdf,
+// need to rework module boundaries to allow more fine-grained control
+const viewForNewCards: CaseReducer<DisplayData, PayloadAction<CardsState>> = (state, { payload }) => {
+	return { ...state, currentView: payload.lastCardIDNumber > 1 ? View.RiverExplorer : View.RiverMaterial };
+};
+const viewForNewPDF: CaseReducer<DisplayData> = (state) => {
+	return { ...state, currentView: View.RiverMaterial };
 };
 
 const displaySlice = createSlice({
@@ -29,7 +38,8 @@ const displaySlice = createSlice({
 		currentView,
 	},
 	extraReducers: {
-		[cards.actions.allCardsReplace.type]: explorerView,
+		[cards.actions.allCardsReplace.type]: viewForNewCards,
+		[pdf.actions.pdfUpload.type]: viewForNewPDF,
 	},
 });
 
